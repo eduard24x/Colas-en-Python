10106be6b040a9fe71cb176098f949731b41d29a
eduard24x <71943757+eduard24x@users.noreply.github.com>
2020-12-12 00:52:00 -0600

    Add files via upload

---
+++ Colas/colas.py
@@ -0,0 +1,16 @@
+#colas
+
+cola=["angel","rosa ","pablo","jesus","miguel","reynel"]
+
+#agregamos elementos al final de la cola
+cola.append("karla")
+cola.append("flor")
+
+print(cola)
+
+#sacando elementos por el principio de la cola
+
+cola.pop(0)
+print(cola)
+
+
